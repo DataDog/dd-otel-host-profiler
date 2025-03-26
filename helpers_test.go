aa937c59be0ebdbf516c4ed07abbf5cce490c64b
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2025-03-26 12:12:55 -0400

    fix: DD_TAGS env var support (#123)
    I noticed that DD_TAGS wasn't working for my doe-bench work and that the
    profiler was outputting the logs shown below despite the fact that the
    tags should be passing the regex checks.
    
    ```
    time="2025-03-26T14:59:52Z" level=warning msg="Rejected user-specified tag 'doe_inputs_hash:557fc418c5996cf91316a93180a53af3b560f22e'"
    time="2025-03-26T14:59:52Z" level=warning msg="Rejected user-specified tag 'doe_start:2025-03-26T14:59:47Z'"
    ```
    
    On closer inspection of the code I noticed a subtle bug causing the
    wrong variable to be referenced.
    
    This commit fixes the bug and adds a test for it.

---
+++ helpers_test.go
@@ -0,0 +1,44 @@
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/DataDog/dd-otel-host-profiler/reporter"
+)
+
+func TestValidateTags(t *testing.T) {
+	tests := []struct {
+		name string
+		tags string
+		want reporter.Tags
+	}{
+		{
+			name: "empty tags",
+			tags: "",
+			want: nil,
+		},
+		{
+			name: "valid tags",
+			tags: "env:dev,service:web",
+			want: reporter.Tags{reporter.MakeTag("env", "dev"), reporter.MakeTag("service", "web")},
+		},
+		{
+			name: "invalid tag",
+			tags: "env:dev,service:web,#invalid:tag",
+			want: reporter.Tags{reporter.MakeTag("env", "dev"), reporter.MakeTag("service", "web")},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := ValidateTags(test.tags)
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("ValidateTags(%q) = %v, want %v", test.tags, got, test.want)
+			}
+		})
+	}
+}
