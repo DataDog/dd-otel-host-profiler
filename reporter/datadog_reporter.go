fab8e5ab588074fa22bf4afd3334f4d073d2a8c0
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2025-08-04 08:27:00 +0000

    backport: Refactor symbol caching
    https://github.com/open-telemetry/opentelemetry-ebpf-profiler/pull/635

--- reporter/datadog_reporter.go
+++ reporter/datadog_reporter.go
@@ -72,17 +72,6 @@ type traceAndMetaKey struct {
 	tid            libpf.PID
 }
 
-// traceEvents holds known information about a trace.
-type traceEvents struct {
-	files              []libpf.FileID
-	linenos            []libpf.AddressOrLineno
-	frameTypes         []libpf.FrameType
-	mappingStarts      []libpf.Address
-	mappingEnds        []libpf.Address
-	mappingFileOffsets []uint64
-	timestamps         []uint64 // in nanoseconds
-}
-
 type processMetadata struct {
 	updatedAt         time.Time
 	executablePath    string
@@ -125,11 +114,8 @@ type DatadogReporter struct {
 	// executables stores metadata for executables.
 	executables *lru.SyncedLRU[libpf.FileID, execInfo]
 
-	// Frames maps frame information to its source location.
-	frames *lru.SyncedLRU[libpf.FrameID, samples.SourceInfo]
-
 	// traceEvents stores reported trace events (trace metadata with frames and counts)
-	traceEvents xsync.RWMutex[map[traceAndMetaKey]*traceEvents]
+	traceEvents xsync.RWMutex[map[traceAndMetaKey]*samples.TraceEvents]
 
 	// processes stores the metadata associated to a PID.
 	processes *lru.SyncedLRU[libpf.PID, processMetadata]
@@ -160,16 +146,6 @@ func NewDatadog(cfg *Config, p containermetadata.Provider) (*DatadogReporter, er
 	}
 	executables.SetLifetime(executableCacheLifetime)
 
-	frames, err := lru.NewSynced[libpf.FrameID, samples.SourceInfo](cfg.FramesCacheElements, libpf.FrameID.Hash32)
-	if err != nil {
-		return nil, err
-	}
-	frames.SetLifetime(framesCacheLifetime)
-	// Using SetLifetime / GetAndRefresh on frames prevents useful information from being
-	// evicted from the cache.
-	// But it also means that active frames will never be evicted, and thus that their corresponding values
-	// (which are maps) may grow indefinitely (cf. https://github.com/open-telemetry/opentelemetry-ebpf-profiler/pull/260).
-
 	processes, err := lru.NewSynced[libpf.PID, processMetadata](cfg.ProcessesCacheElements, libpf.PID.Hash32)
 	if err != nil {
 		return nil, err
@@ -195,9 +171,8 @@ func NewDatadog(cfg *Config, p containermetadata.Provider) (*DatadogReporter, er
 			stopSignal: make(chan libpf.Void),
 		},
 		executables:               executables,
-		frames:                    frames,
 		containerMetadataProvider: p,
-		traceEvents:               xsync.NewRWMutex(map[traceAndMetaKey]*traceEvents{}),
+		traceEvents:               xsync.NewRWMutex(map[traceAndMetaKey]*samples.TraceEvents{}),
 		processes:                 processes,
 		symbolUploader:            symbolUploader,
 		tags:                      createTags(cfg.Tags, runtimeTag, cfg.Version, cfg.EnableSplitByService),
@@ -226,19 +201,14 @@ func (r *DatadogReporter) ReportTraceEvent(trace *libpf.Trace, meta *samples.Tra
 	}
 
 	if tr, exists := (*traceEventsMap)[key]; exists {
-		tr.timestamps = append(tr.timestamps, uint64(meta.Timestamp))
+		tr.Timestamps = append(tr.Timestamps, uint64(meta.Timestamp))
 		(*traceEventsMap)[key] = tr
 		return nil
 	}
 
-	(*traceEventsMap)[key] = &traceEvents{
-		files:              trace.Files,
-		linenos:            trace.Linenos,
-		frameTypes:         trace.FrameTypes,
-		mappingStarts:      trace.MappingStart,
-		mappingEnds:        trace.MappingEnd,
-		mappingFileOffsets: trace.MappingFileOffsets,
-		timestamps:         []uint64{uint64(meta.Timestamp)},
+	(*traceEventsMap)[key] = &samples.TraceEvents{
+		Frames:     trace.Frames,
+		Timestamps: []uint64{uint64(meta.Timestamp)},
 	}
 
 	return nil
@@ -263,27 +233,6 @@ func (r *DatadogReporter) ExecutableMetadata(args *reporter.ExecutableMetadataAr
 	if r.symbolUploader != nil && args.Interp == libpf.Native {
 		r.symbolUploader.UploadSymbols(args.FileID, args.FileName, args.GnuBuildID, args.Open)
 	}
-}
-
-// FrameKnown returns true if the metadata of the Frame specified by frameID is
-// cached in the reporter.
-func (r *DatadogReporter) FrameKnown(frameID libpf.FrameID) bool {
-	_, known := r.frames.GetAndRefresh(frameID, framesCacheLifetime)
-	return known
-}
-
-// FrameMetadata accepts metadata associated with a frame and caches this information.
-func (r *DatadogReporter) FrameMetadata(args *reporter.FrameMetadataArgs) {
-	log.Debugf("FrameMetadata [%x] %v+%v at %v:%v",
-		args.FrameID.FileID(), args.FunctionName, args.FunctionOffset,
-		args.SourceFile, args.SourceLine)
-	si := samples.SourceInfo{
-		LineNumber:     args.SourceLine,
-		FilePath:       args.SourceFile,
-		FunctionOffset: args.FunctionOffset,
-		FunctionName:   args.FunctionName,
-	}
-	r.frames.Add(args.FrameID, si)
 }
 
 // ReportHostMetadata is a NOP for DatadogReporter.
@@ -316,7 +265,6 @@ func (r *DatadogReporter) Start(mainCtx context.Context) error {
 	}, func() {
 		// Allow the GC to purge expired entries to avoid memory leaks.
 		r.executables.PurgeExpired()
-		r.frames.PurgeExpired()
 		r.processes.PurgeExpired()
 	})
 
@@ -385,7 +333,7 @@ func (r *DatadogReporter) reportProfile(ctx context.Context, data *uploadProfile
 		data.containerID, data.entityID, r.family)
 }
 
-func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEvents, start, end time.Time) (*pprofile.Profile, profileStats) {
+func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*samples.TraceEvents, start, end time.Time) (*pprofile.Profile, profileStats) {
 	numSamples := len(hostSamples)
 
 	const unknownStr = "UNKNOWN"
@@ -411,31 +359,32 @@ func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEv
 		sample := &pprofile.Sample{}
 
 		// Walk every frame of the trace.
-		for i := range traceInfo.frameTypes {
-			loc := createPProfLocation(profile, uint64(traceInfo.linenos[i]))
+		for _, uniqueFrame := range traceInfo.Frames {
+			frame := uniqueFrame.Value()
+			loc := createPProfLocation(profile, uint64(frame.AddressOrLineno))
 
-			switch frameKind := traceInfo.frameTypes[i]; frameKind {
+			switch frameKind := frame.Type; frameKind {
 			case libpf.NativeFrame:
 				// As native frames are resolved in the backend, we use Mapping to
 				// report these frames.
 
-				if tmpMapping, exists := fileIDtoMapping[traceInfo.files[i]]; exists {
+				if tmpMapping, exists := fileIDtoMapping[frame.FileID]; exists {
 					loc.Mapping = tmpMapping
 				} else {
-					executionInfo, exists := r.executables.GetAndRefresh(traceInfo.files[i], executableCacheLifetime)
+					executionInfo, exists := r.executables.GetAndRefresh(frame.FileID, executableCacheLifetime)
 
 					// Next step: Select a proper default value,
 					// if the name of the executable is not known yet.
 					var fileName = unknownStr
-					var buildID = traceInfo.files[i].StringNoQuotes()
+					var buildID = frame.FileID.StringNoQuotes()
 					if exists {
 						fileName = executionInfo.fileName
 						buildID = getBuildID(executionInfo.gnuBuildID, executionInfo.goBuildID, buildID)
 					}
 
-					tmpMapping := createPprofMapping(profile, uint64(traceInfo.linenos[i]),
+					tmpMapping := createPprofMapping(profile, uint64(frame.AddressOrLineno),
 						fileName, buildID)
-					fileIDtoMapping[traceInfo.files[i]] = tmpMapping
+					fileIDtoMapping[frame.FileID] = tmpMapping
 					loc.Mapping = tmpMapping
 				}
 				line := pprofile.Line{Function: createPprofFunctionEntry(funcMap, profile, "",
@@ -448,24 +397,15 @@ func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEv
 				// program.
 			default:
 				// Store interpreted frame information as Line message:
-				line := pprofile.Line{}
-
-				if si, exists := r.frames.GetAndRefresh(
-					libpf.NewFrameID(traceInfo.files[i], traceInfo.linenos[i]),
-					framesCacheLifetime); exists {
-					line.Line = int64(si.LineNumber)
-					line.Function = createPprofFunctionEntry(funcMap, profile,
-						si.FunctionName.String(), si.FilePath.String())
-				} else {
-					// At this point, we do not have enough information for the frame.
-					// Therefore, we report a dummy entry and use the interpreter as filename.
-					line.Function = createPprofFunctionEntry(funcMap, profile,
-						"UNRESOLVED", frameKind.String())
+				line := pprofile.Line{
+					Line: int64(frame.SourceLine),
+					Function: createPprofFunctionEntry(funcMap, profile,
+						frame.FunctionName.String(), frame.SourceFile.String()),
 				}
-				loc.Line = append(loc.Line, line)
 
+				loc.Line = append(loc.Line, line)
 				// To be compliant with the protocol generate a dummy mapping entry.
-				loc.Mapping = getDummyMapping(fileIDtoMapping, profile, traceInfo.files[i])
+				loc.Mapping = getDummyMapping(fileIDtoMapping, profile, frame.FileID)
 			}
 			sample.Location = append(sample.Location, loc)
 		}
@@ -477,8 +417,7 @@ func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEv
 		execPath := getExecutablePath(&processMeta, &traceKey)
 
 		// Check if the last frame is a kernel frame.
-		if len(traceInfo.frameTypes) > 0 &&
-			traceInfo.frameTypes[len(traceInfo.frameTypes)-1] == libpf.KernelFrame {
+		if isKernel(traceInfo) {
 			// If the last frame is a kernel frame, we need to add a dummy
 			// location with the kernel as the function name.
 			execPath = "kernel"
@@ -497,7 +436,7 @@ func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEv
 			containerMetadata = processMeta.containerMetadata
 		}
 		if !r.config.Timeline {
-			count := int64(len(traceInfo.timestamps))
+			count := int64(len(traceInfo.Timestamps))
 			labels := make(map[string][]string)
 			addTraceLabels(labels, traceKey, containerMetadata, baseExec, 0)
 			sample.Value = append(sample.Value, count, count*samplingPeriod)
@@ -505,7 +444,7 @@ func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEv
 			profile.Sample = append(profile.Sample, sample)
 		} else {
 			sample.Value = append(sample.Value, 1, samplingPeriod)
-			for _, ts := range traceInfo.timestamps {
+			for _, ts := range traceInfo.Timestamps {
 				sampleWithTimestamp := &pprofile.Sample{}
 				*sampleWithTimestamp = *sample
 				labels := make(map[string][]string)
@@ -514,7 +453,7 @@ func (r *DatadogReporter) createProfile(hostSamples map[traceAndMetaKey]*traceEv
 				profile.Sample = append(profile.Sample, sampleWithTimestamp)
 			}
 		}
-		totalSampleCount += len(traceInfo.timestamps)
+		totalSampleCount += len(traceInfo.Timestamps)
 	}
 
 	profile.DurationNanos = end.Sub(start).Nanoseconds()
@@ -543,7 +482,7 @@ func (r *DatadogReporter) getPprofProfile() {
 	}
 	r.traceEvents.WUnlock(&events)
 
-	entityToSample := make(map[serviceEntity]map[traceAndMetaKey]*traceEvents)
+	entityToSample := make(map[serviceEntity]map[traceAndMetaKey]*samples.TraceEvents)
 
 	if !r.config.EnableSplitByService {
 		profile, stats := r.createProfile(hostSamples, intervalStart, intervalEnd)
@@ -573,8 +512,7 @@ func (r *DatadogReporter) getPprofProfile() {
 			inferredService = true
 		}
 
-		if service == "" && len(traceInfo.frameTypes) > 0 &&
-			traceInfo.frameTypes[len(traceInfo.frameTypes)-1] == libpf.KernelFrame {
+		if service == "" && isKernel(traceInfo) {
 			service = "system"
 		}
 
@@ -591,7 +529,7 @@ func (r *DatadogReporter) getPprofProfile() {
 		}
 		serviceSamples, exists := entityToSample[entity]
 		if !exists {
-			serviceSamples = make(map[traceAndMetaKey]*traceEvents)
+			serviceSamples = make(map[traceAndMetaKey]*samples.TraceEvents)
 			entityToSample[entity] = serviceSamples
 		}
 
@@ -863,4 +801,12 @@ func parseServiceNameFromEnvironData(envData []byte) string {
 
 func getServiceName(pid libpf.PID) string {
 	return getServiceNameFromProcPath(pid, "")
+}
+
+func isKernel(traceInfo *samples.TraceEvents) bool {
+	if len(traceInfo.Frames) == 0 {
+		return false
+	}
+
+	return traceInfo.Frames[len(traceInfo.Frames)-1].Value().Type == libpf.KernelFrame
 }
