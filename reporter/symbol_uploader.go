28deb300374725b76ac1f499af41757920cf5d1c
Nayef Ghattas <nayef.ghattas@datadoghq.com>
2025-08-14 09:44:52 +0000

    Revert "fix(symbol_uploader): reduce parallel uploads"
    This reverts commit f59c34da470f4af584c337767684bdb7a79b2165.

--- reporter/symbol_uploader.go
+++ reporter/symbol_uploader.go
@@ -49,7 +49,7 @@ const (
 	defaultQueryWorkerCount     = 10
 
 	defaultUploadQueueSize   = 1000
-	defaultUploadWorkerCount = 5
+	defaultUploadWorkerCount = 10
 
 	sourceMapEndpoint = "/api/v2/srcmap"
 
