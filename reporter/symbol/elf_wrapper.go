3a7cb42cd0d2b650360efabe9dcc1d2f5a97948c
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2025-08-14 15:32:24 +0200

    bugfix: Do not look for .debug_frame in separate debug files (#196)
    * bugfix: Do not look for .debug_frame in separate debug files
    
    We were looking for .debug_frame in separate debug files, which is not
    present.
    
    * Simplify code

--- reporter/symbol/elf_wrapper.go
+++ reporter/symbol/elf_wrapper.go
@@ -172,13 +172,8 @@ func (e *elfWrapper) findDebugSymbolsWithBuildID() *elfWrapper {
 	}
 
 	// Try to find the debug file
-	debugDirectories := make([]string, 0, len(globalDebugDirectories))
 	for _, dir := range globalDebugDirectories {
-		debugDirectories = append(debugDirectories, filepath.Join(dir, ".build-id"))
-	}
-
-	for _, debugPath := range debugDirectories {
-		debugFile := filepath.Join(debugPath, buildID[:2], buildID[2:]+".debug")
+		debugFile := filepath.Join(dir, ".build-id", buildID[:2], buildID[2:]+".debug")
 		debugELF, err := e.openELF(debugFile)
 		if err != nil {
 			continue
@@ -215,10 +210,6 @@ func (e *elfWrapper) findDebugSymbolsWithDebugLink() *elfWrapper {
 		debugFile := filepath.Join(debugPath, executablePath, linkName)
 		debugELF, err := e.openELF(debugFile)
 		if err != nil {
-			continue
-		}
-		if debugELF.elfFile.Section(".debug_frame") == nil {
-			debugELF.Close()
 			continue
 		}
 		fileCRC32, err := debugELF.elfFile.CRC32()
