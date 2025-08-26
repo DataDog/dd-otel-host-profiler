accd692e0280f296f49907ff6dc21a34ea96bb21
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2025-08-26 15:02:08 +0200

    Update go version to 1.24.6 (#214)
    * Update go version to 1.24.6
    
    * fix: correctly check if debugfs is already mounted

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,13 +1,10 @@
 #! /usr/bin/env sh
-set -e
 
 DD_API_KEY="$(sudo cat /run/secrets/dd-api-key)"
 DD_APP_KEY="$(sudo cat /run/secrets/dd-app-key)"
 export DD_API_KEY DD_APP_KEY
 
-if [ ! -d /sys/kernel/debug/tracing ]; then
-    sudo mount -t debugfs none /sys/kernel/debug
-fi
+sudo test -d /sys/kernel/debug/tracing || sudo mount -t debugfs none /sys/kernel/debug
 
 cd /app/dd-otel-host-profiler
 exec "$@"
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/DataDog/dd-otel-host-profiler
 
-go 1.24.4
+go 1.24.6
 
 require (
 	github.com/DataDog/dd-trace-go/v2 v2.2.2
