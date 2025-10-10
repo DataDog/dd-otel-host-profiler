45ce7bc54622939eba0098ecd26d1a5a8030e62b
Olivier G <olivier.gaca@datadoghq.com>
2025-10-10 17:27:05 +0200

    Revert removal of json support for  SymbolEndpoint

--- reporter/symbol_endpoint.go
+++ reporter/symbol_endpoint.go
@@ -6,7 +6,7 @@
 package reporter
 
 type SymbolEndpoint struct {
-	Site   string `mapstructure:"site"`
-	APIKey string `mapstructure:"api_key"`
-	AppKey string `mapstructure:"app_key"`
+	Site   string `mapstructure:"site" json:"site"`
+	APIKey string `mapstructure:"api_key" json:"api_key"`
+	AppKey string `mapstructure:"app_key" json:"app_key"`
 }
